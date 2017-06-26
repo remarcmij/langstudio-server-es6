aee120b1ba056a32f6379497face58513568761e
Jim Cramer <remarcmij@gmail.com>
2017-06-26 16:07:01 +0200

    some es6-related refactoring

--- src/api/article/articleController.js
+++ src/api/article/articleController.js
@@ -2,13 +2,14 @@
 const ArticleModel = require('./articleModel')
 const log = require('../../services/logService')
 
-const CONTENT_MARKER_REGEXP = /<!-- flashcard -->|<!-- translate-start -->/
+const CONTENT_MARKER_REGEXP = /<!-- flashcard -->/
 const PUBLIC_GROUPS = ['public']
 
 async function getArticle(req, res) {
+  const { user, params } = req
 
   try {
-    const article = await ArticleModel.findOne({ fileName: req.params.filename })
+    const article = await ArticleModel.findOne({ fileName: params.filename })
       .populate('_topic')
       .lean()
       .exec()
@@ -19,17 +20,17 @@ async function getArticle(req, res) {
       article.mdText = ''
     }
 
-    const groups = req.user ? [...req.user.groups, ...PUBLIC_GROUPS] : PUBLIC_GROUPS
-    const isAdmin = req.user && req.user.role === 'admin'
+    const groups = user ? [...user.groups, ...PUBLIC_GROUPS] : PUBLIC_GROUPS
+    const isAdmin = user && user.role === 'admin'
 
     if (!isAdmin && groups.indexOf(article.groupName) === -1) {
-      log.warn(`access denied: ${article.fileName}`, req.user)
+      log.warn(`access denied: ${article.fileName}`, user)
       return res.sendStatus(401)
     }
     res.json(article)
   }
   catch (err) {
-    log.error(`get article error ${err.message}`, req.user)
+    log.error(`get article error ${err.message}`, user)
     res.status(500).send(err.message)
   }
 }
